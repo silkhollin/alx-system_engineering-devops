e1b73d68e9b72d3bc5536540087a376ce4aa2427
silkhollin <silcollinz@gmail.com>
2024-03-27 11:43:15 -0700

    puppet config fix final

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
-#!/usr/bin/env bash
-#config management with puppet
+# Configure ssh config file
 
-file { 'etc/ssh/ssh_config':
-	ensure => present,
-content => "
-	#actual ssh config 
-	host*
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	",
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true
+}
 
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true
 }
