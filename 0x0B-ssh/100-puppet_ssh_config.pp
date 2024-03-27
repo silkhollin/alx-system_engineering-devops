6e21f464506b101555f1d819896d02c06486e693
silkhollin <silcollinz@gmail.com>
2024-03-27 11:31:57 -0700

    puppet config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#config management with puppet
+
+file { 'etc/ssh/ssh_config':
+	ensure => presen,
+content => "
+	#actual ssh config 
+	host*
+	IdentityFile ~/.ssh/school
+	Passwordauthentication no
+
+}
