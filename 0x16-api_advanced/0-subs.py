6f6ed61b9f89880c73eb5f5ff005d1dd2e6262d0
silkhollin <silcollinz@gmail.com>
2024-06-04 17:14:06 +0000

    advanced querring

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-""" Function that queries the Reddit API """
+"""
+function that queries Reddit API and returns
+number of subscribers
+"""
+import json
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """  Args:
-        subreddit: subreddit name
-    Returns:
-        number of subscribers to the subreddit,
-        or 0 if subreddit requested is invalid"""
-    headers = {'User-Agent': 'xica369'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        return (response.json().get("data").get("subscribers"))
-    return (0)
+    """ return number of subscribers """
+    url = requests.get("https://www.reddit.com/r/{}/about.json"
+                       .format(subreddit), headers={"User-Agent": "koco"})
+    if url.status_code == 200:
+        return url.json().get("data").get("subscribers")
+    else:
+        return 0
