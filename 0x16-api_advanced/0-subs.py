3fe52f4cd8a953a4af914b98b9e34d69dd841798
silkhollin <silcollinz@gmail.com>
2024-06-04 17:29:48 +0000

    fixing erros

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """
-function that queries Reddit API and returns
-number of subscribers
+This function queries the reddit api to
+find all the subscribers of a subreddit
 """
-import json
+
 import requests
+# https://www.reddit.com/
 
 
 def number_of_subscribers(subreddit):
-    """ return number of subscribers """
-    url = requests.get("https://www.reddit.com/r/{}/about.json"
-                       .format(subreddit), headers={"User-Agent": "koco"})
-    if url.status_code == 200:
-        return url.json().get("data").get("subscribers")
-    else:
-        return 0
+    headers = {"User-Agent": "Koco"}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    subreddit_info = requests.get(url, headers=headers, allow_redirects=False)
+    if subreddit_info.status_code == 200:
+        data = subreddit_info.json()
+        result_data = data["data"]
+        return (result_data["subscribers"])
+    return (0)
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-""" Function that queries the Reddit API for the top ten reddits """
-import requests
-import sys
+"""
+This function queries the reddit api to
+find all the subscribers of a subreddit
+"""
 
+import requests
+# https://www.reddit.com/
 
 def top_ten(subreddit):
-    """ Returns: top ten post titles
-        or None if queried subreddit is invalid """
-    headers = {'User-Agent': 'xica369'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    toppers = {'limit': 10}
-    response = requests.get(url, headers=headers, allow_redirects=False,
-                            params=toppers)
-
-    if response.status_code == 200:
-        titles_ = response.json().get('data').get('children')
-        for title_ in titles_:
-            print(title_.get('data').get('title'))
+    headers = {"User-Agent": "Koco"}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?Limit=10'
+    subreddit_info = requests.get(url, headers=headers, allow_redirects=False)
+    if subreddit_info.status_code == 200:
+        data = subreddit_info.json()
+        result_data = data["data"]["children"]
+        for item in result_data[:10]:
+            print(item['data']['title'])
     else:
         print(None)
